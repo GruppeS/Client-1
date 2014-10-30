e6e2972a6ce6ef7732091554b643bc77a3c1c761
Sullemain <sullea92@gmail.com>
2014-10-30 13:19:41 +0100

    Nye klasser tilføjet

---
+++ src/gui/Screen.java
@@ -0,0 +1,72 @@
+package gui; // en del af ui
+
+// importerer de nødvendige pakker fra swing og cardlayout
+import java.awt.CardLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Screen extends JFrame // Screen klasse der extender JFrame
+{
+	// instansvariable
+	private static final long serialVersionUID = 1L; // id for screen
+	
+	//final static strenge til at kalde de korresponderende paneler
+	public static final String ADMINADD = "1";
+	public static final String ADMINEDIT = "2";
+	public static final String ADMINPANEL = "3";
+	public static final String LOGINPANEL = "4";
+	public static final String USERDEPOSIT = "5";
+	public static final String USERPANEL = "6";
+	public static final String USERPAY = "7";
+	public static final String USERTRANSFER = "8";
+	public static final String USERWITHDRAW = "9";
+	public static final String RECIPIENTPANEL = "10";
+
+	// variable til at holde på objekter af JPanel
+	private JPanel contentPane;
+	private LoginPanel loginPanel;
+	
+	
+	CardLayout c; //variabel til at holde på cardlayout
+
+	/**
+	 * Constructer that holds the frame and an object for every panel
+	 */
+	public Screen() // konstruktør
+	{
+		setTitle("Bitcoin@CBS"); // titel på vindue
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Programmet lukkes ned når vinduet lukkes
+		setBounds(100, 100, 336, 519); // størrelsen på vinduet
+		contentPane = new JPanel(); // der oprettes et tomt panel
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0)); // størrelsen på vinduet
+		setContentPane(contentPane); // det tomme panel sættes i vinduet
+		contentPane.setLayout(new CardLayout(0, 0)); // panelets layout sættes som et cardlayout
+
+		// Der laves objekter af alle paneler og disse tilføjes contentpane, sammen med et kaldenavn
+
+		loginPanel = new LoginPanel();
+		contentPane.add(loginPanel, LOGINPANEL);
+
+		c = (CardLayout) getContentPane().getLayout(); // cardlayoutet sættes til kunne bestå af de forskellige contentpanes
+	} // konstruktør slutter
+
+	
+	// getters og setters til paneler
+
+	/**
+	 * @return loginPanel
+	 */
+	public LoginPanel getLoginPanel() {
+		return loginPanel; // returnerer loginPanel
+	}
+
+	/**
+	 * @param card
+	 */
+	public void show(String card) // metode til at skifte mellem de forskellige paneler gennem cardlayout
+	{
+		c.show(getContentPane(), card); // cardlayout viser den contentpane der bliver sendt gennem argumentet
+	} // metoden slutter
+} // klassen slutter
