f52f7de7c8a2a8db4bd08221b4f21eb56c2a73ad
Sullemain <sullea92@gmail.com>
2014-12-05 15:49:17 +0100

    impoteret json klasser fra server

---
+++ src/model/Events.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Events {
+	
+	@SuppressWarnings("unused")
+	private String overallID = "getEvents";
+	public ArrayList<Event> events = new ArrayList<Event>();
+ 
+    public ArrayList<Event> getEvents() {
+        return events;
+    }
+}
